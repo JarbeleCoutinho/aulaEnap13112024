3cde62745c5b5dd7658e24d1d10be679a178a06e
JarbeleCoutinho <jarbele.coutinho@ufersa.edu.br>
2024-11-13 11:48:00 -0300

    questao01.py

--- questao01.py
+++ questao01.py
@@ -11,4 +11,5 @@ st.caption("Esta é a a legenda")
 st.code("x=2021")
 st.latex(r''' a+a r^1+a r^2+a r^3 ''')
 
-st.slider('Selecione um número', min_value = 0, max_value = 100)
+resposta = st.slider('Informe o grau de satisfação:', min_value=0, max_value=100)
+st.write(str(resposta))
